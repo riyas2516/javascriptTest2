00c7fa40376776beb13430e54b302a2be1e8f774
riyas2516 <101321494+riyas2516@users.noreply.github.com>
2022-05-19 15:06:01 +0530

    Add files via upload

---
+++ secondTest.js/secondTest.js
@@ -0,0 +1,39 @@
+// ************************prime numnber********************************
+function prime(num){
+    if(num===2){
+
+        return true;
+    }else if(num>1){
+        for(i=2;i<num;i++){
+            if(num%i !==0){
+                return true
+            }else if(num===i*i){
+               return false
+            }else{
+            return false
+            }
+
+        }
+    }else{
+        return false;
+    }
+}
+console.log(prime(3))
+
+// *******************************************************
+function uppercaseFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  console.log(uppercaseFirstLetter('john')); 
+//   *********************************************************
+function secLargestNumber(arr){
+    arr.sort();
+    for(i=arr.length- 2;i>=0;i--){
+        if(arr[i] != arr[i-1]){
+
+        }
+    }
+}
+arr=[12,35,1,10,34,1];
+console.log(secLargestNumber(arr));
